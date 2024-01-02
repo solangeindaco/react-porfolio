02368e5fea785e2c8302eccd0339e9fa5a706719
SolangeI <solange.indaco@gmail.com>
2024-01-01 16:34:56 -0800

    Added the header and the footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
-import { useState } from 'react'
 import './App.css'
-import Nav from './components/Nav'
+import Header from './components/Header'
+import Footer from './components/Footer'
 import { Outlet } from 'react-router-dom'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <Nav />
+      <Header />
       <main className="mx-3">
         <Outlet />
       </main>
+      <Footer />
     </>
   )
 }
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -6,7 +6,7 @@ function Nav() {
   const currentPage = useLocation().pathname;
 
   return (
-    <ul className="nav nav-tabs">
+    <ul className="nav">
       <li className="nav-item">
         <Link
           to="/"
