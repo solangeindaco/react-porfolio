87911cc6023e8854f907850c5a697ededd751b14
SolangeI <solange.indaco@gmail.com>
2024-01-05 19:07:16 -0800

    added info to the Resume page

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -2,17 +2,31 @@ export default function Resume() {
     return (
       <div>
         <h1>Resume</h1>
-          <a href="./src/assets/files/solange_indaco_resume.pdf" download>
-                resume
-          </a>     
-        <p>
-          Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
-          Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
-          mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
-          lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-          imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
-          in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
-        </p>
+          <p>Download my
+            <a href="./src/assets/files/solange_indaco_resume.pdf" download>
+                  resume
+            </a> 
+          </p>
+        <h1>Front-end Proficiencies</h1>
+          <ul>
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>JavaScript</li>
+            <li>JQuery</li>
+            <li>responsive design</li>
+            <li>React</li>
+            <li>Bootstrap</li>
+          </ul>
+        <h1>Back-end Proficiencies</h1>
+          <ul>
+            <li>APIs</li>
+            <li>Node</li>
+            <li>Express</li>
+            <li>MySQL, Sequelize</li>
+            <li>MongoDB, Mongoose</li>
+            <li>REST</li>
+            <li>GraphQL</li>    
+          </ul>
       </div>
     );
   }
