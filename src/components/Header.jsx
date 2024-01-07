086c06fbfff0f5a00e3be00a8b2730676ef21db2
SolangeI <solange.indaco@gmail.com>
2024-01-06 20:27:00 -0800

    Changed colors

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,11 +5,16 @@ const style = {
   backgroundColor: '#BBB5BD',
 }
 
+const nameStyle = {
+  fontSize: '2em',
+  color: '#3D2B3D',
+} 
+
 
 function Header() {
   return (
-    <header className ="mb-20 h-50 d-flex flex-row align-center justify-content-around align-items-center" style={style}>
-      <h1>Solange Indaco</h1>
+    <header className ="mb-20 p-2 d-flex flex-row align-center justify-content-around align-items-center" style={style}>
+      <h1 style={nameStyle} >Solange Indaco</h1>
       <Nav />
     </header>
   );
--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -1,12 +1,23 @@
 import mePhoto from '../assets/images/me-photo.jpg';
 
+const photoStyle = {
+  borderRadius: "65px 65px 65px 65px",
+  /* Adds shadow effec to the photo*/
+  boxShadow: "5px 5px 5px #BBB5BD",
+  /* Set line style of the border of the photo*/
+  borderStyle: "solid",
+   /* Set the width of the border of the photo*/
+  borderWidth: "1px",
+  /* Set the color of the border of the photo*/
+  borderColor: "#BBB5BD",
+}
 
 export default function AboutMe() {
     return (
       <div className='d-flex flex-row align-items-center justify-content-betweem'>
         <div className="d-flex flex-column align-items-start">
           <h1>About Me</h1>
-          <img className="w-25 h-25 rounded-circle" src={mePhoto} alt="Solange Indaco" />
+          <img style={photoStyle} className="w-25 h-25 rounded-circle" src={mePhoto} alt="Solange Indaco" />
         </div>
         <p>Software engineer with almost 4 years of professional experience on Android, Java, PostgreSQL, Groovy and Grails. 
            I also launched a full mobile android game as a personal project with thousands of downloads.
